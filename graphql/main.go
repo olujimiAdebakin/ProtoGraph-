9185c351f8b4b45cea1c9efeaaa8ee2f6cbdfd84
olujimiAdebakin <omoladebu231@gmail.com>
2025-12-07 23:57:27 +0100

    feat(graphql): Setup HTTP server and playground

--- graphql/main.go
+++ graphql/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/playground"
+	_"github.com/99designs/gqlgen/handler"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/kelseyhightower/envconfig"
+)
 
 type AppConfig struct {
 	AccountServiceURL string `envconfig:"ACCOUNT_SERVICE_URL" default:"http://localhost:8081"`
@@ -16,11 +24,15 @@ func main() {
 	}
 
 
-	NewGraphQlServer(cfg.AccountServiceURL, cfg.CatalogServiceURL, cfg.OrderServiceURL)
+	s, err := NewGraphQlServer(cfg.AccountServiceURL, cfg.CatalogServiceURL, cfg.OrderServiceURL)
 	if err != nil {
 		log.Fatalf("Failed to create GraphQL server: %v", err)
 	}
 
+	// s := server
 
-	http.Handler("graphql", )
+	http.Handle("/graphql", handler.New(s.ToExecutableSchema()))
+	http.Handle("/playground", playground.Handler("GraphQL Playground", "/graphql"))
+	log.Println("GraphQL server is running on http://localhost:8080/playground")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
