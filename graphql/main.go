615d33b6cc7432a564974aae793e0e004260c476
olujimiAdebakin <omoladebu231@gmail.com>
2025-12-08 00:50:01 +0100

    refactor: clarify GraphQL server initialization in main.go

--- graphql/main.go
+++ graphql/main.go
@@ -1,12 +1,53 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+
+	
+// 	"github.com/99designs/gqlgen/graphql/playground"
+// 	_"github.com/99designs/gqlgen/handler"
+// 	"github.com/99designs/gqlgen/graphql/handler"
+// 	"github.com/kelseyhightower/envconfig"
+// )
+
+// type AppConfig struct {
+// 	AccountServiceURL string `envconfig:"ACCOUNT_SERVICE_URL" default:"http://localhost:8081"`
+// 	CatalogServiceURL string `envconfig:"CATALOG_SERVICE_URL" default:"http://localhost:8082"`
+// 	OrderServiceURL   string `envconfig:"ORDER_SERVICE_URL" default:"http://localhost:8083"`
+// }
+
+// func main() {
+// 	var cfg AppConfig
+// 	err := envconfig.Process("", &cfg)
+// 	if err != nil {
+// 		log.Fatalf("Failed to process envconfig: %v", err)
+// 	}
+
+
+// 	s, err := NewGraphQlServer(cfg.AccountServiceURL, cfg.CatalogServiceURL, cfg.OrderServiceURL)
+// 	if err != nil {
+// 		log.Fatalf("Failed to create GraphQL server: %v", err)
+// 	}
+
+// 	// s := server
+
+// 	http.Handle("/graphql", handler.New(s.ToExecutableSchema()))
+// 	http.Handle("/playground", playground.Handler("GraphQL Playground", "/graphql"))
+// 	log.Println("GraphQL server is running on http://localhost:8080/playground")
+	
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
 package main
 
 import (
 	"log"
 	"net/http"
 
-	"github.com/99designs/gqlgen/graphql/playground"
-	_"github.com/99designs/gqlgen/handler"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/kelseyhightower/envconfig"
 )
 
@@ -23,16 +64,24 @@ func main() {
 		log.Fatalf("Failed to process envconfig: %v", err)
 	}
 
-
+	// Initialize your GraphQL server, capturing both server instance and error
 	s, err := NewGraphQlServer(cfg.AccountServiceURL, cfg.CatalogServiceURL, cfg.OrderServiceURL)
 	if err != nil {
 		log.Fatalf("Failed to create GraphQL server: %v", err)
 	}
 
-	// s := server
+	// Create executable schema from your server resolvers
+	execSchema := s.ToExecutableSchema()
 
-	http.Handle("/graphql", handler.New(s.ToExecutableSchema()))
+	// Setup the GraphQL handler with the executable schema
+	srv := handler.New(execSchema)
+
+	// Register the GraphQL endpoint
+	http.Handle("/graphql", srv)
+
+	// Register Playground UI at /playground for easy testing
 	http.Handle("/playground", playground.Handler("GraphQL Playground", "/graphql"))
+
 	log.Println("GraphQL server is running on http://localhost:8080/playground")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
